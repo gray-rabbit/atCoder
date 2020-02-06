680ba95d510665f90f1bad38abbfac4e5941d95c
gray-rabbit <talksis@nate.com>
2020-02-06 15:31:19 +0900

    J

--- DP_EDUCATION/dp_j.cpp
+++ DP_EDUCATION/dp_j.cpp
@@ -1,60 +1,50 @@
 #include <bits/stdc++.h>
 #define N 100
 #define INF 1e9 + 5
-#define MO 1e9+7
+#define MO 1e9 + 7
 #define LINF 1e18 + 5
-#define rep(i, N) for(int i=0;i<N;i++)
-#define repp(i,N) for(int i=1;i<=N;i++)
+#define rep(i, N) for (int i = 0; i < N; i++)
+#define repp(i, N) for (int i = 1; i <= N; i++)
 using namespace std;
 using ll = long long;
 
 int n;
-double dp[3001][3001];
-bool chk[3001][3001];
-double coins[3000];
-double solve(int y, int x){
-    if(x<0) return 0.0;
-    if(y==0) return dp[y][x];//이게 기저사례
-    if(y<x) return 0.0;
-    //y번째 동전까지 던졌을 때, x개의 헤드가 나오는 것을 반환한다?
-    double& res = dp[y][x];
-    if(chk[y][x]) return dp[y][x];
-    
-    res = solve(y-1,x-1)*coins[y]+ solve(y-1,x)*(1-coins[y]);
-    chk[y][x]=1;
-    return res;
-}
-int main(){
-    cin>>n;
-    //memset(dp, -1.0, sizeof(dp));
-    for(int i =1;i<=n;i++){
-        scanf("%lf", coins+i);
-    }
-    dp[0][0] = 1.0;
-    double ans =0.0;
-    for(int j=0;j<=n;j++){
-        solve(n,j);
-    }
-    for(int j=0;j<=n;j++){
-        if(j>n-j) ans += dp[n][j];
-    }
-    printf("%.15f\n",ans);
+double dp[302][302][302];
+bool chk[302][302][302];
+int sushi[4];
 
-    return 0;
-    // for(int i=1;i<=n;i++){
-    //     for(int j=0;j<=i;j++){
-    //         if(j-1>=0){
-    //             dp[i][j] += dp[i-1][j-1]*coins[i];
-    //         }
-    //         dp[i][j] += dp[i-1][j] * (1-coins[i]);
-    //     }
-    // }
-    // double ans = 0.0;
-    // for(int i =0;i<=n;i++){
-    //     if(i>n-i) ans += dp[n][i];
-    // }
-    // cout<<ans;
-    //solve(n,n);
-    //cout<<dp[3][2];
+double solve(int a, int b, int c)
+{
+    if (a == 0 && b == 0 && c == 0)
+        return 0.0;
+    if (chk[a][b][c])
+        return dp[a][b][c];
+    double res = 0.0;
+    //아무것도 없는 접시를 선택하는 경우를 생각한다.
+    res += (n - a - b - c) * 1.0 / (a + b + c) * 1.0;
+    //1개 있는 접시를 선택하는 경우를 생각하자.
+    if (a > 0)
+        res += (solve(a - 1, b, c) + 1) * a / (a + b + c) * 1.0;
+    //2개 있는 접시를 선택하는 경우를 생각하자.
+    if (b > 0)
+        res += (solve(a + 1, b - 1, c) + 1) * b / (a + b + c) * 1.0;
+    //3개 있는 접시를 선택하는 경우를 생각하자.
+    if (c > 0)
+        res += (solve(a, b + 1, c - 1) + 1) * c / (a + b + c) * 1.0;
+    chk[a][b][c] = 1;
+    return dp[a][b][c] = res;
+}
+
+int main()
+{
+    cin >> n;
+    rep(i, n)
+    {
+        int t;
+        scanf("%d", &t);
+        sushi[t]++;
+    }
+    dp[0][0][0] = 1.0;
+    printf("%.10f", solve(sushi[1], sushi[2], sushi[3]));
     return 0;
 }
